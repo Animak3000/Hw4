6465919cf8b6a4f9218f252c7afb9a89edd6c758
Andre Mak <cisspook@aim.com>
2016-11-07 16:20:36 -0800

    validate placeholder added

--- src/views/landingview.php
+++ src/views/landingview.php
@@ -5,24 +5,43 @@ class landingview extends View{
     function render($data){
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
     <head>
         <title>PasteChart</title>
+        <script type='text/javascript'>
+        function validateForm(){
+            var x = document.getElementById('values').value;
+            if (x == null || x == '') {
+                alert('no coordinates found');
+                return false;
+            }
+            /*
+            else if(){
+                alert('coordinates in wrong format');
+                return false;
+            }
+            */
+            else{
+                alert('coordinates accepted');
+                return true;
+            }
+        }
+        </script>
     </head>
     <body>
         <h1>PasteChart</h1>
         <p>Share your data in charts!</p>
-        <form>
-        <div><label for="chart">Chart Title</label>
-        <input type="text" name="title">
+        <form onsubmit="return validateForm()">
+        <div><label for='chart'>Chart Title</label>
+            <input type='text' id='title' name='title'>
         </div><br>
         <div>
-            <textarea rows="50" cols="80" placeholder="Jan,600,5.4
+            <textarea id='values' name='values' rows='50' cols='80' placeholder='Jan,600,5.4
 Feb,450,5.0
-..."></textarea>
+...'></textarea>
         </div>
         <div>
-            <button onclick="">Share</button>
+            <button type='submit'>Share</button>
         </div>
         </form>
     </body>
