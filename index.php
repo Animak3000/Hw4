77ec7bb96d5908be74d37707d9d1a473820cee42
michael's mac <enehnnamudi@gmail.com>
2016-11-14 14:57:20 -0800

    wip

--- index.php
+++ index.php
@@ -10,6 +10,14 @@ use \onepersongroup\hw4\configs\Config;
 use \onepersongroup\hw4\controllers\landcontroller;
 use \onepersongroup\hw4\controllers\chartcontroller;
 
+define ("BASE_URL", "http://localhost/Hw4");
+
+//MySql Constants. Edit when needed.
+define ("SERVERNAME", "localhost");
+define ("USERNAME", "root");
+define ("PASSWORD", "123456qwerty");
+define ("DB", "hw4");
+
 //Determine which controller to use.
 if(isset($_GET['c'])){
     if("chart"==$_GET['c']){
