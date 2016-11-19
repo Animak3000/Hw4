d93519c986363d58cac1624d698a53fa0c332dfc
Andre Mak <cisspook@aim.com>
2016-11-18 16:07:47 -0800

    encoding title in javascript

--- src/views/elements/displayelement.php
+++ src/views/elements/displayelement.php
@@ -5,18 +5,38 @@ class displayelement extends Element{
         if($data['graph'] == 'LineGraph'){?>
             <script type="text/javascript" src="./src/resources/chart.js"></script>
             <script type="text/javascript">
+            var title = <?php echo json_encode($data['title']); ?>;
             graph = new Chart("board", 
-                {"Jan":7, "Feb":20, "Dec":5}, {"title":"Test Chart - Month v Value", "type":"LineGraph"});
+                {"Jan":7, "Feb":20, "Dec":5}, {"title":title, "type":"LineGraph"});
             graph.draw();
             </script><?php
         }
         else if($data['graph'] == 'PointGraph'){?>
             <script type="text/javascript" src="./src/resources/chart.js"></script>
             <script type="text/javascript">
+            var title = <?php echo json_encode($data['title']); ?>;
             graph = new Chart("board", 
-                {"Jan":7, "Feb":20, "Dec":5}, {"title":"Test Chart - Month v Value", "type":"PointGraph"});
+                {"Jan":7, "Feb":20, "Dec":5}, {"title":title, "type":"PointGraph"});
             graph.draw();
             </script><?php
+        }
+        else if($data['graph'] == 'Histogram'){?>
+            <script type="text/javascript" src="./src/resources/chart.js"></script>
+            <script type="text/javascript">
+            var title = <?php echo json_encode($data['title']); ?>;
+            graph = new Chart("board", 
+                {"Jan":7, "Feb":20, "Dec":5}, {"title":title, "type":"Histogram"});
+            graph.draw();
+            </script><?php
+        }
+        else if($data['graph'] == 'xml'){
+
+        }
+        else if($data['graph'] == 'json'){
+            
+        }
+        else if($data['graph'] == 'jsonp'){
+            
         }
     }
 }
