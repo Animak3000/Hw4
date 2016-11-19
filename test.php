25ad4e2fbfc35056931a4ec38b97edc88c79b6e3
Andre Mak <cisspook@aim.com>
2016-11-18 23:10:25 -0800

    completed test cases

--- test.php
+++ test.php
@@ -3,25 +3,49 @@ use onepersongroup\hw4\controllers;
 require_once(dirname(__FILE__) . '/vendor/simpletest/simpletest/autorun.php');
 require_once 'vendor/autoload.php';
 
-class checkFormPass extends UnitTestCase {
+class checkFormCoordinates extends UnitTestCase {
     function teststandard() {
         $testfile = new onepersongroup\hw4\controllers\landcontroller;
         $this->assertTrue($testfile->checkForm("title","B,1,1,1,1,1"));
     }
-    function testmissingcoordinate() {
+    function testMissingCoordinate() {
         $testfile = new onepersongroup\hw4\controllers\landcontroller;
         $this->assertTrue($testfile->checkForm("title","B,1,1,,1,1"));
     }
-}
-
-class checkFormFail extends UnitTestCase {
-    function testmissingfirst() {
+    function testMissingFirst() {
         $testfile = new onepersongroup\hw4\controllers\landcontroller;
         $this->assertFalse($testfile->checkForm("title",",1,1,1,1"));
     }
-    function testnewlinechar() {
+    function testNewlineChar() {
         $testfile = new onepersongroup\hw4\controllers\landcontroller;
-        $this->assertFalse($testfile->checkForm("title","E,1,1,1,1,1 \n,1,1,1,1,1"));
+        $this->assertFalse($testfile->checkForm("title","E,1,1,1,1,1\n,1,1,1,1,1"));
+    }
+    function testZeroCoordinates() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertFalse($testfile->checkForm("title","B"));
+    }
+    function testSixCoordinates() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertFalse($testfile->checkForm("title","B,1,1,1,1,1,1"));
+    }
+}
+
+class checkFormLengths extends UnitTestCase {
+    function test80characters() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertTrue($testfile->checkForm("title","BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB,1"));
+    }
+    function test81characters() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertFalse($testfile->checkForm("title","BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB,11"));
+    }
+    function test50lines() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertTrue($testfile->checkForm("title","B,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1"));
+    }
+    function test51lines() {
+        $testfile = new onepersongroup\hw4\controllers\landcontroller;
+        $this->assertFalse($testfile->checkForm("title","B,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1\nB,1,1,1,1,1"));
     }
 }
 ?>
